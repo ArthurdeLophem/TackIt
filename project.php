0cc3eeffdc12318921fdd7d1f38cabef14d66010
r0808 <arthurdelophem3@gmail.com>
2022-05-08 22:50:56 +0200

    fix: dataset syntax in project.php page

--- project.php
+++ project.php
@@ -29,16 +29,16 @@
     
     <div style="height: 70vh; background-color: rgba(255,0,0,0.1); display: flex; justify-content: space-around">
         <div class="itemPicker h-100 d-flex flex-wrap justify-content-center align-content-start items" style="width: 25%; background-color: rgba(0,0,255,.1)">
-            <div data-itemId="0" class="item inactive"></div>
-            <div data-itemId="1" class="item inactive"></div>
-            <div data-itemId="2" class="item inactive"></div>
-            <div data-itemId="3" class="item inactive"></div>
-            <div data-itemId="4" class="item inactive"></div>
-            <div data-itemId="5" class="item inactive"></div>
-            <div data-itemId="6" class="item inactive"></div>
-            <div data-itemId="7" class="item inactive"></div>
-            <div data-itemId="8" class="item inactive"></div>
-            <div data-itemId="9" class="item inactive"></div>
+            <div data-item-id="0" class="item inactive"></div>
+            <div data-item-id="1" class="item inactive"></div>
+            <div data-item-id="2" class="item inactive"></div>
+            <div data-item-id="3" class="item inactive"></div>
+            <div data-item-id="4" class="item inactive"></div>
+            <div data-item-id="5" class="item inactive"></div>
+            <div data-item-id="6" class="item inactive"></div>
+            <div data-item-id="7" class="item inactive"></div>
+            <div data-item-id="8" class="item inactive"></div>
+            <div data-item-id="9" class="item inactive"></div>
         </div>
 
         <div id="map" class="h-100 d-inline-block" style="width: 60%;">
