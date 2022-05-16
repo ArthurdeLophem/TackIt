288fb495f7293b39d1ba88ea584d4069bb9308a4
r0808 <arthurdelophem3@gmail.com>
2022-05-17 00:08:45 +0200

    feat: add projectresult frontend

---
+++ projectResult.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <title>TackIt</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-between">
+        <div class="p-2">
+            <a class="navbar-brand" href="#">logo</a>
+        </div>
+
+        <div class="p-2">
+            <span>Gemeente Tienen</span>
+        </div>
+
+        <div class="p-2">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bell" viewBox="0 0 16 16">
+                <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zM8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z"/>
+            </svg>
+        </div>
+    </nav>
+
+    <div class="container d-flex flex-column">       
+        <div class="shadow p-3 my-5 bg-body rounded bg-image">
+            <div
+                style=" z-index: 0; height: 70%; width: 70%; background-image: url('./css/images/confetti-overlay.png'); background-position: center center; background-size: cover; background-repeat: no-repeat; border-radius: 3px 0px 0px 3px; position: absolute;">
+            </div>
+            <div class="align-self-end flex-column d-flex ">
+        
+                <div class="rounded shadow-1-strong align-self-center mt-5 mb-2"
+                    style="height: 250px; width: 50%; background-image: url('./css/images/project_back.jpg'); background-position: center center; background-size: cover; background-repeat: no-repeat;">
+                </div>
+
+                <div class="align-self-center mb-5" style="z-index: 1;"><h1>project bloemenpark</h1></div>
+
+                <li class="d-flex flex-column justify-content-center align-items-center" style="border-radius: 0; z-index: 1;">
+                    <h2> <strong> winnaar </strong></h2>
+                    <span><strong>Geert Duhoux</strong></span> 
+                </li>
+                <div class="d-flex flex-row justify-content-between" style="z-index: 1;">
+                    <div class="badge bg-dark text-light">159 deelnemers</div>
+                    <div class="badge bg-dark text-light">X votes</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
