af1dc8aa9b82ae6c04ba28a45323ae119561c38d
R0754975 <r0754975@student.thomasmore.be>
2022-05-19 16:44:30 +0200

    gemeente dashboard updated - navigation updated

--- inc/navdefiner.inc.php
+++ inc/navdefiner.inc.php
@@ -1,12 +1,24 @@
 <?php   
 
+session_start();
+$user = $_SESSION['user'];
+$userType = $user['Type'];
+
+if($userType == 0){
+    $dashboard = "citizenDashboard.php";
+}
+if ($userType == 1){
+    $dashboard = "GemeenteDashboard.php";
+}
+
+
 if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')   
 $url = "https://";   
 else  
 $url = "http://";   
 $url = $_SERVER['REQUEST_URI'];    
 
-if($url ='/tackit/citizenDashboard.php') {
+if($url ='/tackit/citizenDashboard.php' || $url ='/tackit/GemeenteDashboard.php') {
     $navClassA = 'selectedTab';
 } else {
     $navClassB = '';
---
+++ inc/topnav.inc.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light d-flex justify-content-between">
+        <div class="p-2">
+            <a class="navbar-brand" href="#"><img src="https://res.cloudinary.com/dgypufy9k/image/upload/v1652810434/Tackit_Assets/logo_1_wt0q6w.png" alt=""></a>
+        </div>
+
+        <div class="p-2">
+            <h1>Gemeente Tienen</h1>
+        </div>
+
+        <div class="p-2">
+            <img src="https://res.cloudinary.com/dgypufy9k/image/upload/v1652970841/Tackit_Assets/Bell_gnlmov.png" alt="">
+        </div>
+    </nav>
