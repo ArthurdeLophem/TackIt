e811fd19711c4d4cf34a1a2ca9d13dc6df9055eb
r0808 <arthurdelophem3@gmail.com>
2022-05-22 10:28:29 +0200

    feat: add items.php with base functions

---
+++ src/core/Items.php
@@ -0,0 +1,124 @@
+<?php
+
+    namespace tackit\core;
+
+    use Exception;
+    use tackit\Data\DB;
+    use PDO;
+
+
+    class Items {
+
+    protected $userId;
+    protected $projectId;
+    protected $items;
+    
+
+    
+
+    /**
+     * Get the value of userId
+     */ 
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * Set the value of userId
+     *
+     * @return  self
+     */ 
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of projectId
+     */ 
+    public function getProjectId()
+    {
+        return $this->projectId;
+    }
+
+    /**
+     * Set the value of projectId
+     *
+     * @return  self
+     */ 
+    public function setProjectId($projectId)
+    {
+        $this->projectId = $projectId;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of items
+     */ 
+    public function getItems()
+    {
+        return $this->items;
+    }
+
+    /**
+     * Set the value of items
+     *
+     * @return  self
+     */ 
+    public function setItems($items)
+    {
+        $this->items = $items;
+
+        return $this;
+    }
+
+    public static function findItemsByProject($projectId){
+        $conn = DB::getConnection();
+        $statement = $conn->prepare("select * from project_items where project_id = :projectId");
+        $statement->bindValue(':projectId', $projectId);
+        $results = $statement->execute();
+        return $results;
+    }
+
+    public static function itemValidation($userId, $projectId){
+        $conn = DB::getConnection();
+        $statement = $conn->prepare("select * from project_items where project_id = :projectId and user_id = :userId");
+        $statement->bindValue(':projectId', $projectId);
+        $statement->bindValue(':userId', $userId);
+        $statement->execute();
+        if ($statement){
+            $response = [
+                "status" => "failed",
+                "message" => "already has items"
+            ];
+        }
+        else {
+            $response = [
+                "status" => "success",
+                "message" => "can save items"
+            ];
+        }
+        echo json_encode($response);
+    }
+
+    public function saveItems(){
+        $conn = DB::getConnection();
+        $statement = $conn->prepare("insert into project_items (items, project_id, user_id) values (:items, :project_id, :user_id)");
+        $statement->bindValue(':items', $this->items);
+        $statement->bindValue(':projectId', $this->projectId);
+        $statement->bindValue(':userId', $this->userId);
+        $statement->execute();
+    }
+
+    public function getProjectStatus($userId, $projectId){
+        $conn = DB::getConnection();
+        $statement = $conn->prepare("select status from project_items where project_id = :projectId and user_id = :userId");
+        $statement->bindValue(':projectId', $projectId);
+        $statement->bindValue(':userId', $userId);
+        return $statement->execute();
+    }
+}
