c358c0134ebeb31016ce885541f3361f9df30331
r0808 <arthurdelophem3@gmail.com>
2022-06-10 14:24:13 +0200

    feat: update database export

--- database/tackit.sql
+++ database/tackit.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.2
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Jun 09, 2022 at 08:25 PM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
+-- Host: localhost:3306
+-- Generation Time: Jun 10, 2022 at 12:23 PM
+-- Server version: 5.7.24
+-- PHP Version: 8.0.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -39,6 +39,13 @@ CREATE TABLE `project` (
   `Type` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `project`
+--
+
+INSERT INTO `project` (`id`, `name`, `start_date`, `end_date`, `budget`, `start_date_cocreatie`, `start_date_voting`, `end_date_cocreatie_voting`, `Type`) VALUES
+(1, 'mijlpaal imd', '2022-06-13 00:00:00', '2022-06-30 00:00:00', 1000, '2022-06-13 00:00:00', '2022-06-15 00:00:00', '2022-06-29 00:00:00', 'imdstud');
+
 -- --------------------------------------------------------
 
 --
@@ -47,7 +54,7 @@ CREATE TABLE `project` (
 
 CREATE TABLE `project_items` (
   `id` int(11) NOT NULL,
-  `items` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`items`)),
+  `items` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin,
   `user_id` int(11) NOT NULL,
   `project_id` int(11) NOT NULL,
   `status` varchar(255) NOT NULL,
@@ -59,7 +66,10 @@ CREATE TABLE `project_items` (
 --
 
 INSERT INTO `project_items` (`id`, `items`, `user_id`, `project_id`, `status`, `budget`) VALUES
-(8, '[{\"itemType\": \"boom\", \"coordinates\": {\"lat\": 50.81462244629948, \"lng\": 4.885061749853978}}, {\"itemType\": \"fontein\", \"coordinates\": {\"lat\": 50.81435833697735, \"lng\": 4.885372658123429}}]', 1, 1, 'finished', '');
+(8, '[{\"coordinates\":{\"lat\":50.81462244629948,\"lng\":4.885061749853978},\"itemType\":\"boom\"},{\"coordinates\":{\"lat\":50.81435833697735,\"lng\":4.885372658123429},\"itemType\":\"fontein\"},{\"coordinates\":{\"lat\":51.02591259031871,\"lng\":4.4776235873646195},\"itemType\":\"fontein\"}]', 1, 1, 'finished', ''),
+(9, '[{\"coordinates\":{\"lat\":51.026032738777424,\"lng\":4.477630913224858},\"itemType\":\"boom\"}]', 5, 1, 'finished', 'eenbudget'),
+(10, '[{\"coordinates\":{\"lat\":51.026075028320534,\"lng\":4.477810621228856},\"itemType\":\"bank\"},{\"coordinates\":{\"lat\":51.02588445431762,\"lng\":4.477604091134709},\"itemType\":\"fontein\"}]', 6, 1, 'finished', 'eenbudget'),
+(11, '[{\"coordinates\":{\"lat\":51.026012628069566,\"lng\":4.477689921823186},\"itemType\":\"boom\"},{\"coordinates\":{\"lat\":51.025921557282274,\"lng\":4.4778428077370345},\"itemType\":\"struik\"},{\"coordinates\":{\"lat\":51.02586421632437,\"lng\":4.477590680089635},\"itemType\":\"straatlamp\"}]', 7, 1, 'finished', 'eenbudget');
 
 -- --------------------------------------------------------
 
@@ -84,7 +94,10 @@ INSERT INTO `users` (`id`, `username`, `email`, `password`, `ProfilePicture`, `T
 (1, 'burger', 'test@test.com', '$2y$12$0aEeqD8GSuVMM3a/ML.7D.gcIIDNMzE/jF3NvamIR8SWMiMEVqTsS', NULL, 0),
 (2, 'testGemeente', 'test2@test.com', '$2y$12$nG/HZmGWP6qoItPCBzxXxeAAfz2chYG.bwbYKpMDYsy4nulv6PqPK', NULL, 1),
 (3, 'testB', 'testB@test.com', '$2y$12$.lWQbnLSsBBB1SJDvt2SM.GtFoo1mxHsWXyf7JiSGHv75igHygBTC', NULL, 0),
-(4, 'testG', 'testG@test.com', '$2y$12$g2DIRHfSkwnnqqsvbzMb9uQpGLXyrCKFcs0TbSvKIvH63dSybh34y', NULL, 1);
+(4, 'testG', 'testG@test.com', '$2y$12$g2DIRHfSkwnnqqsvbzMb9uQpGLXyrCKFcs0TbSvKIvH63dSybh34y', NULL, 1),
+(5, 'adl', 'adl@gmail.com', '$2y$12$Yt//eY8X6f0Vo/xIRsfDt.QQZtYDci/WT.y.TAoAfzQyCZyyHooZe', NULL, 0),
+(6, 'dllaaaa', 'aaddd@g.com', '$2y$12$n3rdq1WHLVnAA4QpUaUY8eZ.421vGBBBJ.JLWHe9TAsCmqekh./ca', NULL, 0),
+(7, 'imag', 'imag@gmail.com', '$2y$12$bn8KXYQrLv1oROnIVgNUnelvVNqYikilu.WMRtyZFeTYOSDTWokzq', NULL, 0);
 
 -- --------------------------------------------------------
 
@@ -98,6 +111,26 @@ CREATE TABLE `vereisten` (
   `amount` int(11) NOT NULL,
   `project_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `votes`
+--
+
+CREATE TABLE `votes` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `voter_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `votes`
+--
+
+INSERT INTO `votes` (`id`, `user_id`, `voter_id`) VALUES
+(73, 1, 7),
+(74, 5, 7);
 
 --
 -- Indexes for dumped tables
@@ -128,6 +161,12 @@ ALTER TABLE `vereisten`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `votes`
+--
+ALTER TABLE `votes`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -135,25 +174,31 @@ ALTER TABLE `vereisten`
 -- AUTO_INCREMENT for table `project`
 --
 ALTER TABLE `project`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `project_items`
 --
 ALTER TABLE `project_items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `vereisten`
 --
 ALTER TABLE `vereisten`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `votes`
+--
+ALTER TABLE `votes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=76;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
