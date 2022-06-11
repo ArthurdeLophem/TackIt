fe6477669a7296f0b5fc87b7471370cc72c27c38
r0808 <arthurdelophem3@gmail.com>
2022-06-11 23:05:44 +0200

    feat: add project info and ui elements on projectview

--- viewProject.php
+++ viewProject.php
@@ -1,5 +1,8 @@
 <?php
 include_once("inc/navdefiner.inc.php");
+require_once(__DIR__ . "/vendor/autoload.php");
+use tackit\core\Votes;
+use tackit\core\Burger;
 
 if(isset($_GET['projectId']) && isset($_GET['userId'])){
     $userProjects = "set";
@@ -8,7 +11,6 @@ if(isset($_GET['projectId']) && isset($_GET['userId'])){
 }
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -33,8 +35,23 @@ if(isset($_GET['projectId']) && isset($_GET['userId'])){
             </div>
         </div>
     <?php else : ?>
-        <div class="map" style="height: 80vh; display: flex; justify-content: space-around; position: relative; top: 120px">
-            <div data-user-id="<?php echo $_GET['userId']; ?>" data-project-id="<?php echo $_GET['projectId']; ?>" id="map" class="shadow h-100 d-inline-block" style="width: 55%; border: 5px solid white; border-radius: 10px;"></div>       
+        <div class="map d-flex flex-column justify-content-center align-items-center" style="height: 90vh; position: relative; top: 70px">
+            <div data-user-id="<?php echo $_GET['userId']; ?>" data-project-id="<?php echo $_GET['projectId']; ?>" id="map" class="shadow h-75 d-inline-block" style="width: 55%; border: 5px solid white; border-radius: 10px;"></div>  
+            <div class="d-flex flex-row justify-content-center align-items-center w-100" style="gap: 10px;">
+                <div class="my-2">
+                    <a type="button" class="btn btn-outline-primary renderBtn d-flex flex-column align-items-center px-4 py-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-camera-video-fill" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M0 5a2 2 0 0 1 2-2h7.5a2 2 0 0 1 1.983 1.738l3.11-1.382A1 1 0 0 1 16 4.269v7.462a1 1 0 0 1-1.406.913l-3.111-1.382A2 2 0 0 1 9.5 13H2a2 2 0 0 1-2-2V5z"/>
+                        </svg>    
+                        <strong>render</strong>
+                    </a>
+                </div>
+                <div class="my-2 d-flex flex-row justify-content-between align-items-center bg-white w-50 px-4 py-2 h-75 rounded">
+                    <p> <?php echo Burger::getUserById($_GET['userId'])['username']?></p>
+                    <p>€4000 budget resterend</p>
+                    <p> <?php echo Votes::getProjectVotes($_GET['userId']) ?> votes</p>
+                </div>
+            </div>     
         </div>
     <?php endif; ?>
     <script src="./scripts/drawmap.js"></script>
