1e79d39fb7eefbb21530d0dc0577e4b3226a9870
r0808 <arthurdelophem3@gmail.com>
2022-06-12 15:04:20 +0200

    feat: township can see all projects and manage each

--- projecten.php
+++ projecten.php
@@ -1,6 +1,12 @@
 <?php
 include_once("inc/navdefiner.inc.php");
-$posts = [1,2,3,4,5,6,7,8,9,10];
+require_once(__DIR__ . "/vendor/autoload.php");
+use tackit\core\Project;
+use tackit\core\Items;
+
+if (isset($_SESSION["user"])) {
+    $projects = Project::getAllProjects();
+}
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
@@ -42,26 +48,30 @@ $posts = [1,2,3,4,5,6,7,8,9,10];
 </div>
 
 <section class="projectListings">
-        <?php foreach($posts as $post): ?>
+        <?php foreach($projects as $project): ?>
             <div class="projectListing">
                 <div class="projectListing-banner" style="background: url('https://res.cloudinary.com/dgypufy9k/image/upload/v1652982240/Tackit_Assets/image_1_3_au2xq7.png');">
                 </div>
                 <div class="projectListing-title">
                     <ul>
-                        <li>Project: zwaluwenstraat</li>
-                        <li>Fase 2: cocreatie</li>
+                        <li><?php echo $project['name']?></li>
+                        <?php if ($project['start_date_cocreatie'] == "2022-06-13 00:00:00") : ?>
+                            <li>Fase 2: cocreatie</li>
+                        <?php elseif($project['start_date_voting'] == "2022-06-14 00:00:00") : ?>
+                            <li>Fase 3: voting</li>
+                        <?php endif; ?>
                     </ul>
                 </div>
                 <div class="projectGListing-info">
                     <ul>
                         <li>
                             <img src="https://res.cloudinary.com/dgypufy9k/image/upload/v1654609817/Tackit_Assets/users_v6ieti.png" alt="I">
-                            <p>45</p>
+                            <p><?php echo count(Items::findAllItemsByProject($project['id']))?></p>
                             <p>Participanten</p>
                         </li>
                         <li>
                             <img src="https://res.cloudinary.com/dgypufy9k/image/upload/v1654609817/Tackit_Assets/approve_file_um7l5f.png" alt="I">
-                            <p>22</p>
+                            <p><?php echo count(Items::findAllItemsByProject($project['id']))?></p>
                             <p>Voltooide Creaties</p>
                         </li>
                         <li>
@@ -72,7 +82,15 @@ $posts = [1,2,3,4,5,6,7,8,9,10];
                     </ul>
                 </div>
                 <div class="projectListing-button">
-                    <a href=""><img src="https://res.cloudinary.com/dgypufy9k/image/upload/v1654609817/Tackit_Assets/Setting_gycl0c.png" alt="button"></a>
+                    <?php if ($project['start_date_cocreatie'] == "2022-06-13 00:00:00") : ?>    
+                        <a href=""><img src="https://res.cloudinary.com/dgypufy9k/image/upload/v1654609817/Tackit_Assets/Setting_gycl0c.png" alt="button"></a>
+                    <?php elseif($project['start_date_voting'] == "2022-06-14 00:00:00") : ?>
+                        <a href="projectVoting.php?projectId=<?php echo $project['id']?>">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="white" class="bi bi-check-square-fill" viewBox="0 0 16 16">
+                            <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z"/>
+                            </svg>
+                        </a>
+                    <?php endif; ?>
                 </div>
             </div>
             <?php endforeach; ?>
